1d75888023179c79c575e6e6c19c87299eb4bffb
DoriaFabio <doriafabio2001@gmail.com>
2026-05-28 18:08:30 +0200

    remove votazioni

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import DefaultLayout from './pages/DefaultLayout'
 import Homepage from './pages/Homepage'
 import TeamDetail from './pages/TeamDetail'
 import Meme from './pages/meme'
-import Votazioni from './pages/Votazioni'
 import Trofei from './pages/Trofei'
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
           <Route path='/' Component={Homepage} />
           <Route path='/team/:teamIndex' Component={TeamDetail} />
           <Route path='/meme' Component={Meme} />
-          <Route path='/votazioni' Component={Votazioni} />
           <Route path='/trofei' Component={Trofei} />
         </Route>
       </Routes>
